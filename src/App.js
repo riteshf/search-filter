44eb8b2ac9ec09d612984ed95863adb479dc0a30
Ritesh Firodiya <ritesh@Riteshs-MacBook-Pro.local>
2022-01-11 15:17:51 +0530

    added react again

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { ChakraProvider, Container, Grid } from "@chakra-ui/react";
-
-import { SearchOptions } from "./components/SearchOptions/SearchOptions";
 import response from "./__data/Shops_attributes_response.json";
-import { FilterProvider } from "./context/FilterContext";
+import { ChakraProvider, Container, Grid } from "@chakra-ui/react";
+import { SearchFilter } from "@search-filter/react";
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -13,9 +11,7 @@ function App() {
       <Container maxW="container.xl" mt={8}>
         <Grid templateColumns="1fr 1fr 1fr">
           <div className="col bg-white">
-            <FilterProvider options={options}>
-              <SearchOptions />
-            </FilterProvider>
+            <SearchFilter options={options} />
           </div>
           <div className="col">Column</div>
           <div className="col">Column</div>
